788d53691e9b7ad639786027c5ae68147dd8958f
ZeshanWD <zeeshan9choudhary@gmail.com>
2021-09-08 21:19:34 +0200

    setup bootstrap and react-router

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Header from '../components/Header';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>This is Home!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
